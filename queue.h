8c15729c9799513b535b8ddd3d71290c021026f7
Aditya Chebrolu <53321066+superadi04@users.noreply.github.com>
2023-04-26 18:00:40 -0500

    Create queue.h

---
+++ queue.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <stdnoreturn.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+#include "hashmap.h"
+
+typedef struct Queue {
+    struct Interpreter *head; 
+    struct Interpreter *tail;
+} Queue;
+
+void addQ(Queue* q, struct Interpreter* r) {
+    r->next = 0;
+    if (q->tail != 0) {
+        q->tail->next = r;
+    }
+    q->tail = r;
+
+    if (q->head == 0) {
+        q->head = r;
+    }
+}
+
+struct Interpreter* removeQ(Queue* q) {
+    struct Interpreter* r = q->head;
+    if (r != 0) {
+        q->head = r->next;
+        if (q->tail == r) {
+            q->tail = 0;
+        }
+    }
+    return r;
+}
